cac8cca50d9e0cd6557b0318eb4c47f5198bfb70
John Alexander <jalexander@vivantech.com>
2015-02-17 13:00:10 -0800

    foundation files

---
+++ src/main/java/org/kuali/kra/question/CopyPeriodsQuestion.java
@@ -0,0 +1,28 @@
+package org.kuali.kra.question;
+
+import java.util.ArrayList;
+
+import org.kuali.rice.kns.question.QuestionBase;
+
+
+public class CopyPeriodsQuestion extends QuestionBase {
+    
+    public static final String ONE = "1";
+    public static final String ALL = "0";
+    
+    private static final ArrayList<String> BUTTONS;
+    static {
+        BUTTONS = new ArrayList<String>();
+        BUTTONS.add("copyoneper"); 
+        BUTTONS.add("copyallperiods");
+    }
+    
+    /**
+     * @param question
+     * @param buttons
+     */
+    public CopyPeriodsQuestion() {
+        super("Confirm", BUTTONS);
+    }
+
+}
